ef5da10bcdf2d6a2bf3b23e62863f27c4e4db705
TaoZeyu <i@taozeyu.com>
2025-07-30 18:05:43 +0800

    feat(subflows): add folder-downloader

---
+++ subflows/folder-downloader/scriptlets/+scriptlet#1.py
@@ -0,0 +1,38 @@
+from pathlib import Path
+from oocana import Context
+
+#region generated meta
+import typing
+class Inputs(typing.TypedDict):
+  files: dict
+  saved_folder: str | None
+  headers: dict
+  cookies: dict
+class Outputs(typing.TypedDict):
+  tasks: list[dict]
+  saved_folder: str
+#endregion
+
+
+def main(params: Inputs, context: Context) -> Outputs:
+  saved_folder = params["saved_folder"]
+  task_jsons: list[dict] = []
+
+  if saved_folder is None:
+    saved_folder = Path(context.session_dir) / "downloader"
+  else:
+    saved_folder = Path(saved_folder)
+
+  saved_folder.mkdir(parents=True, exist_ok=True)
+
+  for file in params["files"]:
+    task_jsons.append({
+      "url": file["url"],
+      "saved_path": str(saved_folder / file["name"]),
+      "headers": params["headers"],
+      "cookies": params["cookies"]
+    })
+  return {
+    "tasks": task_jsons,
+    "saved_folder": str(saved_folder),
+  }
